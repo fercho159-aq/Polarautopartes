08720da4321c5a3e3f4fb74ba2a04baeaa08cace
Trejo Torres Fernando <fernando.trejo.enp5.159@gmail.com>
2025-08-24 04:22:21 +0000

    sigue mal pues la busqueda se pone encima de los elementos

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
 
+
 'use client';
 
 import Link from 'next/link';
@@ -26,7 +27,7 @@ export default function Header() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 max-w-screen-2xl items-center justify-between px-[25px]">
+      <div className="container flex h-16 max-w-screen-2xl items-center justify-between px-4 sm:px-6 md:px-8 lg:px-6">
         <Link href="/" className="flex items-center gap-2">
           <Wrench className="h-6 w-6 text-primary" />
           <span className="font-headline text-xl font-bold text-primary">Polar Autopartes</span>
